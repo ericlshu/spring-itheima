59488a4d0d88b05818bcb1e999f91dcbf807cf0a
Eric SHU <shuli0502@gmail.com>
2022-03-08 13:44:37 +0800

    day03 - Spring MVC Hello World;

---
+++ spring_ssm/spring_mvc/src/main/java/com/eric/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.eric.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Description :
+ *
+ * @author Eric L SHU
+ * @date 2022-03-08 13:33
+ */
+@Controller
+public class UserController {
+    @RequestMapping("/quick")
+    public String save()
+    {
+        System.out.println("save user in controller ...");
+        return "success.jsp";
+    }
+}
