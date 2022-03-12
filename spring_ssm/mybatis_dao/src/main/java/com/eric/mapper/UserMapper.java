fbaf122bdf1ad53641d6c57ae0b32eb2ac850212
Eric SHU <shuli0502@gmail.com>
2022-03-12 21:05:12 +0800

    day11 - Mybatis traditional DAO;

--- spring_ssm/mybatis_dao/src/main/java/com/eric/mapper/UserMapper.java
+++ spring_ssm/mybatis_dao/src/main/java/com/eric/mapper/UserMapper.java
@@ -2,7 +2,6 @@ package com.eric.mapper;
 
 import com.eric.domain.User;
 
-import java.io.IOException;
 import java.util.List;
 
 /**
@@ -14,8 +13,16 @@ import java.util.List;
 public interface UserMapper {
     /**
      * find all user
+     *
      * @return user list
-     * @throws IOException IOException
      */
-    List<User> findAll() throws IOException;
+    List<User> findAll();
+
+    /**
+     * find user by id
+     *
+     * @param id id
+     * @return user
+     */
+    User findById(int id);
 }
