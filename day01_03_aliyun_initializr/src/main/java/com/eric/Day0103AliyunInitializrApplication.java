af430fcc3a9ebf70a06be73d2e7be00bf2ef9efd
Eric SHU <shuli0502@gmail.com>
2022-03-18 15:10:53 +0800

    Quick start 03 with Aliyun initializr;

---
+++ day01_03_aliyun_initializr/src/main/java/com/eric/Day0103AliyunInitializrApplication.java
@@ -0,0 +1,17 @@
+package com.eric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Eric SHU
+ */
+@SpringBootApplication
+public class Day0103AliyunInitializrApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Day0103AliyunInitializrApplication.class, args);
+    }
+
+}
---
+++ day01_03_aliyun_initializr/src/main/java/com/eric/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.eric.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description :
+ *
+ * @author Eric L SHU
+ * @date 2022-03-18 14:45
+ */
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @GetMapping
+    public String getById()
+    {
+        String result = "spring boot quick start 3 ......";
+        System.out.println("result = " + result);
+        return result;
+    }
+}
