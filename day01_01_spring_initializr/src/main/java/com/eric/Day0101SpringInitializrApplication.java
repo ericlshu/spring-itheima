d29cf0532c8971aad89dfa7cf68bb6aea30104e3
Eric SHU <shuli0502@gmail.com>
2022-03-18 14:51:03 +0800

    Quick start 01 with Spring Initializr;

---
+++ day01_01_spring_initializr/src/main/java/com/eric/Day0101SpringInitializrApplication.java
@@ -0,0 +1,17 @@
+package com.eric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Eric SHU
+ */
+@SpringBootApplication
+public class Day0101SpringInitializrApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Day0101SpringInitializrApplication.class, args);
+    }
+
+}
