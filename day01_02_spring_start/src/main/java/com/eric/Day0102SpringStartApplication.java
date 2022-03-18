a547f4ab65f09a8478bbf6b0c43004b6c1b574e7
Eric SHU <shuli0502@gmail.com>
2022-03-18 14:57:22 +0800

    Quick start 02 with https://start.spring.io/;

---
+++ day01_02_spring_start/src/main/java/com/eric/Day0102SpringStartApplication.java
@@ -0,0 +1,16 @@
+package com.eric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Eric SHU
+ */
+@SpringBootApplication
+public class Day0102SpringStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day0102SpringStartApplication.class, args);
+	}
+
+}
