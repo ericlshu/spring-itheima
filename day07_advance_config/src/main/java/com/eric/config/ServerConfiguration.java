537f956e85bb3a8c8374c8d42fa840445bb7be3d
Eric SHU <shuli0502@gmail.com>
2022-03-24 13:34:36 +0800

    Common units of measurement since JDK8;

--- day07_advance_config/src/main/java/com/eric/config/ServerConfiguration.java
+++ day07_advance_config/src/main/java/com/eric/config/ServerConfiguration.java
@@ -2,6 +2,13 @@ package com.eric.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.convert.DataSizeUnit;
+import org.springframework.boot.convert.DurationUnit;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 /**
  * Description :
@@ -19,5 +26,10 @@ public class ServerConfiguration {
     private int port;
     private long timeout;
 
+    @DurationUnit(ChronoUnit.HOURS)
+    private Duration serverTimeOut;
+
+    @DataSizeUnit(DataUnit.MEGABYTES)
+    private DataSize dataSize;
 }
 
