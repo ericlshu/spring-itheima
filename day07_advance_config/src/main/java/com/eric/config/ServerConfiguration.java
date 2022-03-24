610faa0131864930a34f0d1ea8858e6d9d9b0519
Eric SHU <shuli0502@gmail.com>
2022-03-24 13:52:15 +0800

    JSR303 data validation api;

--- day07_advance_config/src/main/java/com/eric/config/ServerConfiguration.java
+++ day07_advance_config/src/main/java/com/eric/config/ServerConfiguration.java
@@ -6,7 +6,10 @@ import org.springframework.boot.convert.DataSizeUnit;
 import org.springframework.boot.convert.DurationUnit;
 import org.springframework.util.unit.DataSize;
 import org.springframework.util.unit.DataUnit;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
@@ -14,15 +17,23 @@ import java.time.temporal.ChronoUnit;
  * Description :
  *
  * @author Eric L SHU
- * @ EnableConfigurationProperties与@ Component不能同时使用
+ * 注： @EnableConfigurationProperties 与 @Component不能同时使用
+ * 3. @Validated开启对当前bean的属性注入校验
  * @date 2022-03-24 11:53
  */
 // @Component
 @Data
 @ConfigurationProperties(prefix = "servers")
+@Validated
 public class ServerConfiguration {
 
     private String ipAddress;
+
+    /**
+     * 4. 设置校验规则
+     */
+    @Max(value = 8888, message = "最大值不能超过8888")
+    @Min(value = 2222, message = "最小值不能低于2222")
     private int port;
     private long timeout;
 
