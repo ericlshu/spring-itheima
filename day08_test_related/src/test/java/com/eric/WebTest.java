2736a19d1502e4cea42c6ba72f85a3b7856b363c
Eric SHU <shuli0502@gmail.com>
2022-03-25 11:41:44 +0800

    Match request by response status;

--- day08_test_related/src/test/java/com/eric/WebTest.java
+++ day08_test_related/src/test/java/com/eric/WebTest.java
@@ -7,7 +7,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
 
 /**
  * Description :
@@ -32,10 +36,34 @@ public class WebTest {
     @Test
     void testMockMvc(@Autowired MockMvc mvc) throws Exception
     {
-        log.warn("com.eric.WebTest.testRandomWeb ...");
+        log.warn("com.eric.WebTest.testMockMvc ...");
         // 创建虚拟请求，当前访问/books
         RequestBuilder builder = MockMvcRequestBuilders.get("/books");
         // 执行请求
         mvc.perform(builder);
+    }
+
+
+    /**
+     * java.lang.AssertionError: Status expected:<200> but was:<404>
+     */
+    @Test
+    void testStatus(@Autowired MockMvc mvc) throws Exception
+    {
+        log.warn("com.eric.WebTest.testStatus ...");
+
+        RequestBuilder builder = MockMvcRequestBuilders.get("/books");
+        ResultActions actions = mvc.perform(builder);
+
+        // 使用本次真实执行结果与预期结果进行比对
+
+        // 1. 通过结果匹配器获得当前模拟运行状态
+        StatusResultMatchers status = MockMvcResultMatchers.status();
+
+        // 2. 定义预期执行状态
+        ResultMatcher statusOk = status.isOk();
+
+        // 3. 添加预期值到本次调用过程中进行匹配
+        actions.andExpect(statusOk);
     }
 }
