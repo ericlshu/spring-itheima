18d8a19145400a534ba5db9f3d57998af0d9e697
Eric SHU <shuli0502@gmail.com>
2022-03-25 21:35:27 +0800

    Springboot internal JdbcTemplate;

--- day09_dao_sql/src/main/java/com/eric/mapper/BookMapper.java
+++ day09_dao_sql/src/main/java/com/eric/mapper/BookMapper.java
@@ -1,15 +1,11 @@
 package com.eric.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.eric.domain.Book;
-import org.apache.ibatis.annotations.Mapper;
-
 /**
  * Description :
  *
  * @author Eric L SHU
  * @date 2022-03-20 13:51
  */
-@Mapper
-public interface BookMapper extends BaseMapper<Book> {
+// @Mapper
+public interface BookMapper { //extends BaseMapper<Book>
 }
--- day09_dao_sql/src/test/java/com/eric/Day09DaoSqlApplicationTests.java
+++ day09_dao_sql/src/test/java/com/eric/Day09DaoSqlApplicationTests.java
@@ -1,23 +1,55 @@
 package com.eric;
 
 import com.eric.domain.Book;
-import com.eric.mapper.BookMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @SpringBootTest
 class Day09DaoSqlApplicationTests {
 
+    // @Autowired
+    // private BookMapper bookMapper;
+
     @Autowired
-    private BookMapper bookMapper;
+    private JdbcTemplate jdbcTemplate;
 
     @Test
     void contextLoads()
     {
-        Book book = bookMapper.selectById(10);
-        log.warn("book : " + book);
+        // Book book = bookMapper.selectById(10);
+        // log.warn("book : " + book);
+    }
+
+    @Test
+    void testJdbcTemplate()
+    {
+        String sql= "insert into tbl_book values (null,?,?,?)";
+        jdbcTemplate.update(sql,"SpringBoot Name","SpringBoot Type","SpringBoot Description");
+
+        sql = "select * from tbl_book";
+        List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
+        // System.out.println("maps = " + maps);
+
+        RowMapper<Book> rowMapper = (rs, rowNum) -> {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            String type = rs.getString("type");
+            String description = rs.getString("description");
+            return new Book(id, name, type, description);
+        };
+
+        List<Book> bookList = jdbcTemplate.query(sql, rowMapper);
+        for (Book book : bookList)
+        {
+            log.warn("book : " + book);
+        }
     }
 }
