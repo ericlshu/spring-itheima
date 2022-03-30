4c24caebedb797fa0e8576df9de06e8d12462e52
Eric SHU <shuli0502@gmail.com>
2022-03-30 11:37:10 +0800

    SpringBoot-J2Cache integration;

--- day15_j2cache/src/main/java/com/eric/service/impl/SimCodeServiceImpl.java
+++ day15_j2cache/src/main/java/com/eric/service/impl/SimCodeServiceImpl.java
@@ -4,6 +4,8 @@ import com.eric.domain.SimCode;
 import com.eric.service.SimCodeService;
 import com.eric.utils.CodeUtils;
 import lombok.extern.slf4j.Slf4j;
+import net.oschina.j2cache.CacheChannel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -21,10 +23,14 @@ public class SimCodeServiceImpl implements SimCodeService {
     @Resource(name = "codeUtils")
     private CodeUtils codeUtils;
 
+    @Autowired
+    private CacheChannel cacheChannel;
+
     @Override
     public String sendCodeToSim(String number)
     {
         String code = codeUtils.generator(number);
+        cacheChannel.set("sim", number, code);
         return code;
     }
 
@@ -32,7 +38,7 @@ public class SimCodeServiceImpl implements SimCodeService {
     public boolean checkCode(SimCode simCode)
     {
         log.warn(simCode.toString());
-        String cacheCode = "";
+        String cacheCode = cacheChannel.get("sim", simCode.getNumber()).asString();
         String paramCode = simCode.getCode();
         return paramCode.equals(cacheCode);
     }
