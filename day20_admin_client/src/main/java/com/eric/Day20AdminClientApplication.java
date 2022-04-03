7acbb53f860bbb1d06264c8d424e6e260554058b
Eric SHU <shuli0502@gmail.com>
2022-04-03 23:18:42 +0800

    Spring-Boot-Admin-Client;

---
+++ day20_admin_client/src/main/java/com/eric/Day20AdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.eric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day20AdminClientApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Day20AdminClientApplication.class, args);
+    }
+}
---
+++ day20_admin_client/src/test/java/com/eric/Day20AdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day20AdminClientApplicationTests
+{
+    @Test
+    void contextLoads()
+    {
+    }
+}
