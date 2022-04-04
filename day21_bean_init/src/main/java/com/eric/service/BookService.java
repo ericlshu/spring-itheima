1aa199ba985a3fbce64d353e8b6639632d1114e0
Eric SHU <shuli0502@gmail.com>
2022-04-04 12:57:25 +0800

    0. Spring Bean init env preparation;

---
+++ day21_bean_init/src/main/java/com/eric/service/BookService.java
@@ -0,0 +1,13 @@
+package com.eric.service;
+
+/**
+ * Description :
+ *
+ * @author Eric L SHU
+ * @date 2022-04-04 12:52
+ * @since jdk-11.0.14
+ */
+public interface BookService
+{
+    void check();
+}
---
+++ day21_bean_init/src/main/java/com/eric/service/impl/BookServiceImpl2.java
@@ -0,0 +1,19 @@
+package com.eric.service.impl;
+
+import com.eric.service.BookService;
+
+/**
+ * Description :
+ *
+ * @author Eric L SHU
+ * @date 2022-04-04 12:52
+ * @since jdk-11.0.14
+ */
+public class BookServiceImpl2 implements BookService
+{
+    @Override
+    public void check()
+    {
+        System.out.println("book service 2 ....");
+    }
+}
