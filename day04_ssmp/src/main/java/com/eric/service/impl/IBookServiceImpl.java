c327f769530a4c3431e9bd03eabe612f86e27592
Eric SHU <shuli0502@gmail.com>
2022-04-04 12:22:58 +0800

    Metrics endpoint configuration;

--- day04_ssmp/src/main/java/com/eric/service/impl/IBookServiceImpl.java
+++ day04_ssmp/src/main/java/com/eric/service/impl/IBookServiceImpl.java
@@ -7,9 +7,12 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.eric.domain.Book;
 import com.eric.mapper.BookMapper;
 import com.eric.service.IBookService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.apache.logging.log4j.util.Strings;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * Description :
@@ -18,10 +21,18 @@ import org.springframework.stereotype.Service;
  * @date 2022-03-20 18:29
  */
 @Service
-public class IBookServiceImpl extends ServiceImpl<BookMapper, Book> implements IBookService {
+public class IBookServiceImpl extends ServiceImpl<BookMapper, Book> implements IBookService
+{
 
-    @Autowired
+    @Resource
     private BookMapper bookMapper;
+
+    private final Counter counter;
+
+    public IBookServiceImpl(MeterRegistry meterRegistry)
+    {
+        counter = meterRegistry.counter("分页查询次数：");
+    }
 
     @Override
     public IPage<Book> getPage(int current, int size)
@@ -32,6 +43,9 @@ public class IBookServiceImpl extends ServiceImpl<BookMapper, Book> implements I
     @Override
     public IPage<Book> getPage(int current, int size, Book book)
     {
+        // 统计分页查询执行的次数，并在spring-boot-admin进行展示
+        counter.increment();
+
         LambdaQueryWrapper<Book> lambdaQueryWrapper = new LambdaQueryWrapper<>();
         lambdaQueryWrapper.like(Strings.isNotEmpty(book.getType()), Book::getType, book.getType());
         lambdaQueryWrapper.like(Strings.isNotEmpty(book.getName()), Book::getName, book.getName());
