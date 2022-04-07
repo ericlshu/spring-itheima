d28f79bca0052fcbe7cc2dda22ec10397e3fda98
Eric SHU <shuli0502@gmail.com>
2022-04-07 22:21:36 +0800

    Change System AutoConfiguration;

--- day23_bean_properties/src/main/java/com/eric/Day23BeanPropertiesApplication.java
+++ day23_bean_properties/src/main/java/com/eric/Day23BeanPropertiesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(excludeName = "org.springframework.boot.autoconfigure.context.LifecycleAutoConfiguration")
 // @Import(TomAndJerry.class)
 public class Day23BeanPropertiesApplication
 {
