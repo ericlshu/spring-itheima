c7d01b310855384764118ce2ba3fdaa4f364b304
Eric SHU <shuli0502@gmail.com>
2022-05-24 23:12:00 +0800

    统一异常处理机制；

---
+++ springmvc_10_exception/src/main/java/com/eric/config/MybatisConfig.java
@@ -0,0 +1,27 @@
+package com.eric.config;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+public class MybatisConfig
+{
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource)
+    {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setTypeAliasesPackage("com.eric.domain");
+        return sqlSessionFactoryBean;
+    }
+
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer()
+    {
+        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+        mapperScannerConfigurer.setBasePackage("com.eric.dao");
+        return mapperScannerConfigurer;
+    }
+}
---
+++ springmvc_10_exception/src/test/java/com/eric/service/BookServiceTest.java
@@ -0,0 +1,33 @@
+package com.eric.service;
+
+import com.eric.config.SpringConfig;
+import com.eric.domain.Book;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
+public class BookServiceTest
+{
+    @Resource
+    private BookService bookService;
+
+    @Test
+    public void testGetById()
+    {
+        Book book = bookService.getById(1);
+        System.out.println(book);
+    }
+
+    @Test
+    public void testGetAll()
+    {
+        List<Book> bookList = bookService.getAll();
+        bookList.forEach(System.out::println);
+    }
+}
