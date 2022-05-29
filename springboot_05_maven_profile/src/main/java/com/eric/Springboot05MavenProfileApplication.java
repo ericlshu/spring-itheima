14a1057b7c6ff7e6113df027ce514ab11c24537c
Eric SHU <shuli0502@gmail.com>
2022-05-29 16:23:50 +0800

    SpringBoot-Maven多环境配置；

---
+++ springboot_05_maven_profile/src/main/java/com/eric/Springboot05MavenProfileApplication.java
@@ -0,0 +1,15 @@
+package com.eric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot05MavenProfileApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Springboot05MavenProfileApplication.class, args);
+    }
+
+}
