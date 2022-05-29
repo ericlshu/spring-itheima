4d8837617c30cf6759f2855e9db97ffb9ef5b275
Eric SHU <shuli0502@gmail.com>
2022-05-29 12:04:21 +0800

    SpringBoot快速入门；

---
+++ springboot_01_quickstart/src/test/java/com/eric/Springboot01QuickstartApplicationTests.java
@@ -0,0 +1,15 @@
+package com.eric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01QuickstartApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
