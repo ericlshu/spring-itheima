cdf2952e91be742d78eb7ef8f479af252a1bf50a
Eric SHU <shuli0502@gmail.com>
2022-05-29 15:57:51 +0800

    SpringBoot多环境配置；

---
+++ springboot_04_profiles/src/test/java/com/eric/Springboot04ProfilesApplicationTests.java
@@ -0,0 +1,15 @@
+package com.eric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot04ProfilesApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
