8bdea7e4c3e67a1340b87bf5792ce22c55cf75c1
Eric SHU <shuli0502@gmail.com>
2022-08-17 22:39:37 +0800

    Tomcat异常之BasicErrorController;

--- src/main/java/com/eric/a32/WebConfig.java
+++ src/main/java/com/eric/a32/WebConfig.java
@@ -1,25 +1,28 @@
 package com.eric.a32;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletRegistrationBean;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.ErrorPageRegistrar;
 import org.springframework.boot.web.server.ErrorPageRegistrarBeanPostProcessor;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.Map;
 
 /**
  * 1. 因为内嵌了 Tomcat 容器，因此可以配置 Tomcat 的错误页面，Filter 与 错误页面之间是通过请求转发跳转的，可以在这里做手脚
@@ -35,6 +38,7 @@ import java.util.Map;
  * -->> 配置了 ErrorViewResolver，根据状态码去找 View
  * -->> 没配置或没找到，用 BeanNameViewResolver 根据一个固定为 error 的名字找到 View，即所谓的 WhitelabelErrorView
  */
+@Slf4j
 @Configuration
 public class WebConfig
 {
@@ -100,12 +104,48 @@ public class WebConfig
             return null;
         }
 
-        @RequestMapping("/error")
+        /*@RequestMapping("/error")
         @ResponseBody
         public Map<String, Object> error(HttpServletRequest request)
         {
             Throwable e = (Throwable) request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
             return Map.of("error", e.getMessage());
-        }
+        }*/
+    }
+
+    /**
+     * ⬅️ErrorProperties 封装环境键值, ErrorAttributes 控制有哪些错误信息
+     */
+    @Bean
+    public BasicErrorController basicErrorController()
+    {
+        ErrorProperties errorProperties = new ErrorProperties();
+        errorProperties.setIncludeException(true);
+        return new BasicErrorController(new DefaultErrorAttributes(), errorProperties);
+    }
+
+    /**
+     * ⬅️名称为 error 的视图, 作为 BasicErrorController 的 text/html 响应结果
+     */
+    @Bean
+    public View error()
+    {
+        return (model, request, response) ->
+        {
+            log.info("model : {}", model);
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().print("""
+                    <h3>服务器内部错误</h3>
+                    """);
+        };
+    }
+
+    /**
+     * ⬅️收集容器中所有 View 对象, bean 的名字作为视图名
+     */
+    @Bean
+    public ViewResolver viewResolver()
+    {
+        return new BeanNameViewResolver();
     }
 }
